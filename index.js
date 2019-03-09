9706f9029ec9b1f88b33a5ea6ded2c0d165d5f29
Victor Laforga <37700441+victorlaforga@users.noreply.github.com>
2019-03-09 16:16:36 +0100

    updated files for the Listing exercise

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 const express = require('express');
 const port = 4999;
-
 const app = express();
+const fakeData = {
 
+  id: 'chicken-pasta',
+  title: 'Pasta with chicken',
+  amount: 'For 4 people',
+  duration: '55 minutes',
+
+}
 
 app.use('/static', express.static('static'))
-app.use(notfound)
 app.set('view engine', 'pug')
-app.get('/', test)
 
+.get('/', index)
+.get('/aboutMe', aboutMe)
 
-function test(req, res) {
+.use(errorPage)
+
+function index(req, res) {
   res.render('index.pug')
 }
 
-function notfound(err, html) {
-  res.render("notfound.pug")
+function aboutMe(req, res) {
+    res.send('This is the about ME page!');
+}
+
+function errorPage(req, res) {
+    res.status(404).render('notfound.pug');
 }
 
 app.listen(port);
