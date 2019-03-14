a35196b9a5be72b547e179a860b9c4c85c6c1551
victorlaforga <victorlaforga@hotmail.com>
2019-03-14 14:23:17 +0100

    added form and updated files

--- index.js
+++ index.js
@@ -1,41 +1,77 @@
 const express = require('express');
-const port = 4999;
-
+const find = require('find');
+const port = process.env.PORT || 4999;
 const app = express();
-const fakeData
+const data = [{
 
-app.use(app.router);
+  id: 'chicken-pasta',
+  title: 'Pasta with chicken',
+  amount: 'For 4 people',
+  duration: '55 minutes',
+  description: 'In a large skillet over medium heat, warm oil and add chicken; cook until slightly brown. Add onion and garlic to cook for about 5 minutes or until garlic is golden and onions are translucent. Add tomatoes, broccoli, salt, pepper and oregano; stir well and bring to a boil. Cover and turn down heat to simmer for about 10 minutes.'
+
+},
+{
+  id: 'scrambled-egg',
+  title: 'Scrambled Egg',
+  amount: 'For 1 person',
+  duration: '15 minutes',
+  description: 'Whisk eggs, salt and pepper in small bowl. Melt butter in non-stick skillet over medium heat. Pour in egg mixture and reduce heat to medium-low. As eggs begin to set, gently move spatula across bottom and side of skillet to form large, soft curds.'
+
+}]
+
 app.use('/static', express.static('static'))
-app.use(notfound)
 app.set('view engine', 'pug')
-app.get('/', test)
+  .get('/', index)
+  .get('/add', addRecipeForm)
+  .get('/aboutMe', aboutMe)
+  .get('/recipe', recipe)
+  .get('/:id', recipeFind)
+  .use(errorPage)
 
-
-function test(req, res) {
+function index(req, res) {
   res.render('index.pug')
 }
 
-function notfound(err, html) {
-  res.render("notfound.pug")
+function aboutMe(req, res) {
+  res.send('This is the about ME page!');
 }
 
-app.use(function(req, res, next){
-  res.status(404);
+function errorPage(req, res) {
+  res.status(404).render('notfound.pug');
+}
 
-  // respond with html page
-  if (req.accepts('html')) {
-    res.render('404', { url: req.url });
-    return;
+function addRecipeForm(req, res) {
+  res.render('add_recipe.pug');
+}
+
+function recipe(req, res) {
+  var length = data.length;
+  var index = -1;
+  var doc = '<!doctype html>'
+  while (++index < length) {
+    recipes = data[index]
+    doc += '<h3><a href="/' + recipes.id + '">' + recipes.title + '</a></h3>'
+    doc += '<p>' + recipes.amount + '</p>'
+    doc += '<p>' + recipes.description + '</p>'
+
   }
 
-  // respond with json
-  if (req.accepts('json')) {
-    res.send({ error: 'Not found' });
-    return;
-  }
+  res.send(doc)
+}
 
-  // default to plain-text. send()
-  res.type('txt').send('Not found');
-});
+function recipeFind(req, res) {
+  var doc = `
+<!doctype html>
+<title>${ recipes.title }</title>
+<h1>${ recipes.title }</h1>
+<p>${ recipes.description }</p>`
+
+
+  res.send(doc)
+
+
+}
+
 
 app.listen(port);
