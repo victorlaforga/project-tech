70060b7ef56f22ec038ca1a934c7302ce92177e1
victorlaforga <victorlaforga@hotmail.com>
2019-03-14 14:23:05 +0100

    added a form and updated files

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const port = 4999;
 
 const app = express();
+const fakeData
 
-
+app.use(app.router);
 app.use('/static', express.static('static'))
 app.use(notfound)
 app.set('view engine', 'pug')
@@ -17,5 +18,24 @@ function test(req, res) {
 function notfound(err, html) {
   res.render("notfound.pug")
 }
+
+app.use(function(req, res, next){
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('404', { url: req.url });
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+});
 
 app.listen(port);
